cd239da86b497d19203179244461c8b2535ceb40
Jacquelyn Sansone <sansonej@oregonstate.edu>
2025-06-08 11:25:07 -0600

    connecting minecraft server to terraform

--- main.tf
+++ main.tf
@@ -10,16 +10,30 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region                   = "us-west-2"
   shared_credentials_files = ["~/.aws/credentials"]
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+ ami		= "ami-04999cd8f2624f834"
+ instance_type 	= "t2.large"
+ key_name 	= "minecraft"
+ subnet_id	= "subnet-0f116fa7a42d0a000"
+ associate_public_ip_address = true
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+ provisioner "remote-exec" {
+  script = "restart-w-reboot.sh"
+ }
+
+ connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/downloads/minecraft.pem")
+    host        = self.public_ip 
+ }
+
+
+ tags = {
+    Name = "Minecraft Server"
+ }
 }
-
